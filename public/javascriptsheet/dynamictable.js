92b091b9e5a1c4e3d4fefdb775c62469d8ec6626
mzhao123 <mzhao8808@gmail.com>
2018-03-08 13:35:35 -0500

    tables are fully functional, added export to excel option

--- public/javascriptsheet/dynamictable.js
+++ public/javascriptsheet/dynamictable.js
@@ -1,3 +1,4 @@
+
 var $TABLE = $('#table');
 var $BTN = $('#export-btn');
 var $EXPORT = $('#export');
@@ -8,11 +9,16 @@ $('.table-add-row').click(function () {
   //clones the tr and removes the "hide" class from it so it is visible
   var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
 
-  var rowCounter = $('#firstTableRow td').length;
-  for (var x = rowCounter-1; x > 0; x--)
+  var rowCounter = $('#firstTableRow th').length;
+  for (var x = rowCounter-3; x > 0; x--)
   {
     console.log(x);
     //inserts new column into the cloned table row based on size in the right place
+    if(x == rowCounter-3)
+    {
+      $clone.find('td').eq(0).before('<td style = "text-align: center;" contenteditable="true" ><b>Attribute 1 </b></td>');
+    }
+    else
     $clone.find('td').eq(0).after('<td style = "text-align: center;" contenteditable="true" >undefined</td>');
   }
   //adds the table row
@@ -22,17 +28,17 @@ $('.table-add-row').click(function () {
 //adds column
 $('.table-add-column').click(function(){
   //finds the td, removes the hide class
-var $clone = $TABLE.find('td.hide').clone(true).removeClass('hide');
+var $clone = $TABLE.find('th.hide').clone(true).removeClass('hide');
 
 //adds the cloned td as a column to the first row
-$ACTUALTABLE.find('tr').eq(0).append($clone);
+$($ACTUALTABLE.find('tr').eq(0).find('th').eq(0)).after($clone);
 console.log($clone);
 
 //goes through each row
 $ACTUALTABLE.find('tr').each(function(){
 var trow = $(this);
 // trow.index is greater than 2 because it skips the first tr and also a tr that is hidden
-if(trow.index() > 2)
+if(trow.index() > 0)
 {
   //inserts the new column
   trow.find('td').eq(0).after('<td style = "text-align: center;" contenteditable="true" >undefined</td>');
@@ -50,10 +56,47 @@ $('.table-delete-column').click(function(){
      //another condition has to be met in order for something to be deleted
      if(!trow.find('td').eq(1).hasClass('no-delete'))
      trow.find('td').eq(1).remove();
+     if(!trow.find('th').eq(1).hasClass('no-delete'))
+     trow.find('th').eq(1).remove();
    }
   })
   })
+  function fnExcelReport()
+  {
+      var tab_text="<table border='2px'><tr bgcolor='#87AFC6'>";
+      var textRange; var j=0;
+      tab = document.getElementById('myTable'); // id of table
 
+      for(j = 0 ; j < tab.rows.length ; j++)
+      {
+        if(j != (tab.rows.length -1) && j!= 1)
+        {
+          tab_text=tab_text+tab.rows[j].innerHTML+"</tr>";
+          //tab_text=tab_text+"</tr>";
+        }
+      }
+
+      tab_text=tab_text+"</table>";
+      tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+      tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
+      tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+      var ua = window.navigator.userAgent;
+      var msie = ua.indexOf("MSIE ");
+
+      if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+      {
+          txtArea1.document.open("txt/html","replace");
+          txtArea1.document.write(tab_text);
+          txtArea1.document.close();
+          txtArea1.focus();
+          sa=txtArea1.document.execCommand("SaveAs",true,"Say Thanks to Sumit.xls");
+      }
+      else                 //other browser not tested on IE 11
+          sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
+
+      return (sa);
+  }
 
 
 $('.table-remove').click(function () {
